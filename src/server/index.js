20add067d176d0de2135216f1b21c879d0a12f0c
“Ori <ori.nahu@gmail.com>
2022-11-04 10:31:08 +0200

    add cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const { apiRouter } = require("./rouets/api");
 const { apiGetPrsRouter } = require("./rouets/get-prs");
 const { apiGetPicRouter } = require("./rouets/get-pr-pic");
@@ -10,6 +12,7 @@ require("./db/mongoose");
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+app.use(cors());
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
