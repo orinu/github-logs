96a035471a0fb950746e889de0d46491f7eb3f42
“Ori <ori.nahu@gmail.com>
2022-11-04 09:37:49 +0200

    change client links

--- src/client/src/components/Header/Header.js
+++ src/client/src/components/Header/Header.js
@@ -12,9 +12,9 @@ const Header = () => {
           <span>Ori Nahum </span>
         </h2>
         <h3 className="bottom-mid-header">
-          <a href="https://github.com/"> Api Server </a>
-          <a href="https://github.com/"> Client</a>
-          <a href="https://github.com/"> Demo Repo</a>
+          <a href="https://github.com/orinu/github-logs/tree/main/src/server"> Api Server </a>
+          <a href="https://github.com/orinu/github-logs/tree/main/src/client"> Client</a>
+          <a href="https://github.com/orinu/github-logs-demo-repo"> Demo Repo</a>
         </h3>
       </div>
       <img src={auditechLogo} alt="logo" />
